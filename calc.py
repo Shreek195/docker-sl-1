c38fb9fffb42cafb48bdf45c684fda9b1fa85635
Shreek195 <shreekoshti199@gmail.com>
2026-07-01 00:41:30 +0530

    Add multiplication feature

--- calc.py
+++ calc.py
@@ -3,9 +3,13 @@ def main():
     print("--- Simple Calculator ---")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    
+
     addition = num1 + num2
+    multiplication = num1 * num2
+    
     print(f"Addition: {num1} + {num2} = {addition}")
+    print(f"Multiplication: {num1} * {num2} = {multiplication}")
+
 
 if __name__ == "__main__":
     main()
